25dfda4fa3b0ba592d863432a56b6cf63790238f
Rajdip27 <srajdip920@gamil.com>
2025-05-21 23:28:58 +0600

    fix

--- src/App/BizFlow.Api/Program.cs
+++ src/App/BizFlow.Api/Program.cs
@@ -1,41 +1,39 @@
 using BizFlow.Application.Extensions;
 using BizFlow.IoC.Configuration;
+using Microsoft.AspNetCore.Builder;
 var builder = WebApplication.CreateBuilder(args);
-
-// Configure services
 builder.Services.AddIOCConfiguration(builder.Configuration);
 builder.Services.AddCors(options =>
 {
-    options.AddDefaultPolicy(policyBuilder =>
-    {
-        policyBuilder.AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader();
+    options.AddDefaultPolicy(builder => {
+        builder.AllowAnyOrigin();
+        builder.AllowAnyMethod();
+        builder.AllowAnyHeader();
     });
 });
 
+// Add services to the container.
+
+
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Global exception handler (put early to catch startup errors)
-app.UseMiddleware<ExceptionMiddleware>();
-
-// Enable Swagger only in development
-if (app.Environment.IsDevelopment())
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment() || !app.Environment.IsDevelopment())
 {
+    // Enable Swagger only in Development environment
     app.UseSwagger();
-    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseMiddleware<ExceptionMiddleware>();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting(); // should be before Auth and Controllers
-
+app.UseRouting();
 app.UseCors();
+app.UseStaticFiles();
+app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
