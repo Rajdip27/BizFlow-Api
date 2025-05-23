ac71735f3af265c8343867f8a27c870c3926e479
Rajdip27 <srajdip920@gamil.com>
2025-05-24 00:00:06 +0600

    Add Outlet View model and Configuration

---
+++ src/BizFlow.Infrastructure/EntityConfiguration/OutletConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using BizFlow.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+namespace BizFlow.Infrastructure.EntityConfiguration;
+public class OutletConfiguration: IEntityTypeConfiguration<Outlet>
+{
+    public void Configure(EntityTypeBuilder<Outlet> builder)
+    {
+        builder.ToTable(nameof(Outlet));
+        builder.HasKey(x => x.Id);
+    }
+}
